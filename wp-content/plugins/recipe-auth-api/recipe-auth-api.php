dd0425bcdb8bbbbec0383014448768601b55213b
morteza <morteza.rostami.cs@gmail.com>
2025-11-21 00:03:37 -0800

    fix cors 02

--- wp-content/plugins/recipe-auth-api/recipe-auth-api.php
+++ wp-content/plugins/recipe-auth-api/recipe-auth-api.php
@@ -80,13 +80,26 @@ add_action('rest_api_init', function () {
     add_filter('rest_pre_serve_request', function ($value) {
         $origin = get_http_origin();
 
-        if ($origin === 'https://recipes.mortteza.site') {
+        $allowed_origins = [
+            'https://recipes.mortteza.site',
+            'https://www.recipes.mortteza.site',
+        ];
+
+        if (in_array($origin, $allowed_origins, true)) {
             header("Access-Control-Allow-Origin: $origin");
             header("Access-Control-Allow-Credentials: true");
         }
 
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Headers: Content-Type, Authorization, X-WP-Nonce");
+
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            header("Access-Control-Allow-Origin: $origin");
+            header("Access-Control-Allow-Credentials: true");
+            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-WP-Nonce");
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+            exit;
+        }
 
         return $value;
     });
